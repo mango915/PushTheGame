452d4d3de257bf7e8c978f66519927d570c29925
Alecompa <alessandro.compagnucci@gmail.com>
2024-02-12 22:09:15 +0100

    initial simple multiplayer demo

---
+++ MultiplayerController.gd
@@ -0,0 +1,88 @@
+extends Control
+
+@export var address = "127.0.0.1"
+@export var port = 8910
+
+var peer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	multiplayer.peer_connected.connect(peer_connected)
+	multiplayer.peer_disconnected.connect(peer_disconnected)
+	multiplayer.connected_to_server.connect(connected_to_server)
+	multiplayer.connection_failed.connect(connection_failed)
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func peer_connected(id):
+	print("Player Connected: " + str(id))
+
+func peer_disconnected(id):
+	print("Player disconnected: " + str(id))
+	GameManager.players.erase(id)
+	var players = get_tree().get_nodes_in_group("Player")
+	for i in players:
+		if i.name == str(id):
+			i.queue_free()
+	
+func connected_to_server():
+	print("Connected to server!")
+	send_player_information.rpc_id(1,$LineEdit.text, multiplayer.get_unique_id())
+
+func connection_failed():
+	print("Connection failed!")
+
+
+@rpc("any_peer")
+func send_player_information(name, id):
+	if !GameManager.players.has(id):
+		GameManager.players[id] = {
+			"name": name,
+			"id": id,
+			"score": 0
+		}
+	if multiplayer.is_server():
+		for i in GameManager.players:
+			send_player_information.rpc(GameManager.players[i].name, i)
+
+@rpc("any_peer","call_local")
+func start_game():
+	var scene = load("res://test_scene.tscn").instantiate()
+	get_tree().root.add_child(scene)
+	self.hide()
+
+func _on_host_button_down():
+	peer = ENetMultiplayerPeer.new()
+	var error = peer.create_server(port, 6)
+	
+	if error != OK:
+		print("cannot host: "+ error)
+		return
+		
+	peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
+	
+	multiplayer.set_multiplayer_peer(peer)
+	print("Waiting For Players")
+	send_player_information($LineEdit.text, multiplayer.get_unique_id())
+	
+	pass # Replace with function body.
+
+
+func _on_join_button_down():
+	peer = ENetMultiplayerPeer.new()
+	peer.create_client(address,port)
+	peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
+	multiplayer.set_multiplayer_peer(peer)
+	
+	pass # Replace with function body.
+
+
+func _on_start_game_button_down():
+	start_game.rpc()
+	
+	pass # Replace with function body.
---
+++ player.gd
@@ -0,0 +1,55 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@export var bullet : PackedScene
+
+func _ready():
+	$MultiplayerSynchronizer.set_multiplayer_authority(str(name).to_int())
+	for i in GameManager.players:
+		if i == $MultiplayerSynchronizer.get_multiplayer_authority():
+			$Label.text = GameManager.players[i].name
+	$AnimatedSprite2D.play("default")
+
+func _physics_process(delta):
+	if $MultiplayerSynchronizer.get_multiplayer_authority() != multiplayer.get_unique_id():
+		return
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	$GunRotation.look_at(get_viewport().get_mouse_position())
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("fire"):
+		fire.rpc()
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+		if direction < 0:
+			$AnimatedSprite2D.play("run")
+			$AnimatedSprite2D.flip_h = false
+		else:		
+			$AnimatedSprite2D.play("run")
+			$AnimatedSprite2D.flip_h = true
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimatedSprite2D.play("default")
+
+	move_and_slide()
+
+@rpc("any_peer","call_local")
+func fire():
+	var bullet = bullet.instantiate()
+	bullet.global_position = $GunRotation/BulletSpawn.global_position
+	bullet.rotation = $GunRotation.rotation
+	get_tree().root.add_child(bullet)
