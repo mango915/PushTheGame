6e5a6f4cf6fa24082b79cc68f353f5aa6826b334
Alecompa <alessandro.compagnucci@gmail.com>
2024-05-05 13:01:19 +0200

    Added melee starting weapon

---
+++ Scripts/melee.gd
@@ -0,0 +1,48 @@
+extends Node2D
+
+@onready var animation_player = $AnimationPlayer
+
+const red_hand = preload("res://Assets/Players/Hands/red_hand.tres")
+const yellow_hand = preload("res://Assets/Players/Hands/yellow_hand.tres")
+const green_hand = preload("res://Assets/Players/Hands/green_hand.tres")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+		
+	if get_parent().get_parent().get_node("MultiplayerSynchronizer").get_multiplayer_authority() != multiplayer.get_unique_id():
+		return
+		
+	if get_parent().get_parent().is_dead:
+		return
+	
+	var dir = get_global_mouse_position() - global_position
+	if dir.x < 0:
+		scale = Vector2( - 1, 1)
+	else:
+		scale = Vector2(1, 1)
+
+	if Input.is_action_just_pressed("fire") and get_parent().get_parent().can_shoot:
+		punch.rpc()
+
+@rpc("any_peer", "call_local")
+func punch():
+	animation_player.play("punch")
+
+
+func _on_area_2d_body_entered(body:Node2D):
+	if body.has_method("take_damage") and multiplayer.is_server():
+		body.take_damage()
+
+
+func set_color(color):
+	if color == "red":
+		$Hand.texture = red_hand
+	elif color == "yellow":
+		$Hand.texture = yellow_hand
+	elif color == "green":
+		$Hand.texture = green_hand
